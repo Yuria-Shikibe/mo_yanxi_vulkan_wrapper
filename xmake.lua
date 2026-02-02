266c93f98803fa56e315b9e0dfd1812e287d1186
Yuria-Shikibe <3415339403@qq.com>
2026-02-02 14:40:25 +0800

    xmake update

--- xmake.lua
+++ xmake.lua
@@ -1,5 +1,3 @@
-local current_dir = os.scriptdir()
-
 includes("external/**/xmake.lua");
 
 add_rules("mode.debug", "mode.release")
@@ -15,42 +13,37 @@ else
     set_runtimes("c++_shared")
 end
 
-function mo_yanxi_vulkan_wrapper_use_vulkan()
-
-    if is_mode("debug") then
-        add_defines("MO_YANXI_VULKAN_WRAPPER_ENABLE_CHECK=1")
-    else
-        add_defines("MO_YANXI_VULKAN_WRAPPER_ENABLE_CHECK=0")
-    end
-
-    mo_yanxi_utility_use_comp()
-
-    add_files(path.join(current_dir, "./src/vulkan_wrapper/**.cpp"))
-    add_files(path.join(current_dir, "./src/vulkan_wrapper/**.ixx"), {public = true})
-
-    add_includedirs(path.join(current_dir, "external/VulkanMemoryAllocator/include"), {public = true})
-
-    local vulkan_sdk = os.getenv("VULKAN_SDK")
-    if not vulkan_sdk then
-        print("Vulkan SDK not found!")
-    else
-        add_includedirs(path.join(vulkan_sdk, "include"), {public = true})
-        add_linkdirs(path.join(vulkan_sdk, "lib"), {public = true})
-        add_links("vulkan-1", {public = true})
-    end
-
-    add_defines("VK_USE_64_BIT_PTR_DEFINES=1", {public = true})
-end
-
 target("mo_yanxi.vulkan_wrapper")
-    set_kind("static")
+    set_kind("object")
     set_languages("c++23")
     set_policy("build.c++.modules", true)
 
     set_warnings("all")
     set_warnings("pedantic")
 
-    mo_yanxi_vulkan_wrapper_use_vulkan()
+    if is_mode("debug") then
+            add_defines("MO_YANXI_VULKAN_WRAPPER_ENABLE_CHECK=1")
+        else
+            add_defines("MO_YANXI_VULKAN_WRAPPER_ENABLE_CHECK=0")
+        end
+
+        add_deps("mo_yanxi.utility")
+
+        add_files("./src/vulkan_wrapper/**.cpp")
+        add_files("./src/vulkan_wrapper/**.ixx", {public = true})
+
+        add_includedirs("external/VulkanMemoryAllocator/include", {public = true})
+
+        local vulkan_sdk = os.getenv("VULKAN_SDK")
+        if not vulkan_sdk then
+            print("Vulkan SDK not found!")
+        else
+            add_includedirs(path.join(vulkan_sdk, "include"), {public = true})
+            add_linkdirs(path.join(vulkan_sdk, "lib"), {public = true})
+            add_links("vulkan-1", {public = true})
+        end
+
+        add_defines("VK_USE_64_BIT_PTR_DEFINES=1", {public = true})
 target_end()
 
 
